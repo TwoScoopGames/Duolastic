8d8113c210f8c0831565f71f77876f2a1a9bcd00
Eric Lathrop <eric@ericlathrop.com>
2017-01-22 11:05:30 -0500

    Extract config and drawShadowCircle

---
+++ src/draw-shadow-circle.js
@@ -0,0 +1,64 @@
+var config = require("./stack-config");
+
+module.exports = function drawShadowCircle(ctx, x, y, r, color, inset, insetColor, outset, outsetColor) {
+  var radius = r + outset;
+  var radgrad = ctx.createRadialGradient(x, y, 0, x, y, radius);
+
+  radgrad.addColorStop(0, parseRGBA(color));
+  var stop = Math.max(r - inset, 0) / radius;
+  radgrad.addColorStop(stop, parseRGBA(color));
+
+  if (inset === 0 && outset === 0) {
+    radgrad.addColorStop(1, colorWithZeroAlpha(color));
+  }
+
+  if (inset > 0) {
+    if (outset === 0) {
+      console.log("stop", stop);
+      stop = 1 - 0.01;
+      radgrad.addColorStop(stop, parseRGBA(insetColor));
+      radgrad.addColorStop(1, colorWithZeroAlpha(insetColor));
+    } else {
+      stop += inset / radius;
+      radgrad.addColorStop(stop, parseRGBA(insetColor));
+    }
+  }
+
+  if (outset > 0) {
+    stop += 0.0001;
+    radgrad.addColorStop(stop, parseRGBA(outsetColor));
+    radgrad.addColorStop(1, colorWithZeroAlpha(outsetColor));
+  }
+
+  ctx.fillStyle = radgrad;
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+  ctx.fill();
+  if (config.debugLines) {
+    ctx.strokeStyle = parseRGBA(config.debugLineColor);
+    ctx.strokeRect(x - radius, y - radius, radius * 2, radius * 2);
+  }
+};
+
+function parseRGBA(colors) {
+  if (typeof colors === "string") {
+    return colors;
+  }
+  var c = [
+    Math.round(colors[0]),
+    Math.round(colors[1]),
+    Math.round(colors[2]),
+    colors[3]
+  ];
+  return "rgba(" + c.join(",") + ")";
+}
+
+function colorWithZeroAlpha(colors) {
+  var c = [
+    Math.round(colors[0]),
+    Math.round(colors[1]),
+    Math.round(colors[2]),
+    0
+  ];
+  return "rgba(" + c.join(",") + ")";
+}
---
+++ src/stack-config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  perspective: 0.5,
+  offsetX: 2,
+  offsetY: 32,
+  shadowOffsetX: 1.2,
+  shadowOffsetY: -6,
+  outsetSize: 10,
+  outsetColor: [0, 0, 0, 0.5],
+  insetSize: 10,
+  insetColor: [0, 0, 0, 1],
+  debugLines: false,
+  debugLineColor: [0, 0, 0, 1],
+  colors: {
+    "standard": [
+      "rgba(62,88,137,0)",
+      "rgba(76,128,175,1)",
+      "rgba(85,188,221,1)",
+      "rgba(147,213,186,1)",
+      "rgba(228,240,128,1)",
+      "rgba(249,229,114,1)",
+      "rgba(255,207,92,1)",
+      "rgba(255,156,31,1)",
+      "rgba(241,81,57,1)",
+      "rgba(198,55,47,1)",
+      "rgba(142,40,62,1)",
+      "rgba(89,49,75,1)"
+    ],
+    "inverted": [
+      "rgba(89,49,75,1)",
+      "rgba(142,40,62,1)",
+      "rgba(198,55,47,1)",
+      "rgba(241,81,57,1)",
+      "rgba(255,156,31,1)",
+      "rgba(255,207,92,1)",
+      "rgba(249,229,114,1)",
+      "rgba(228,240,128,1)",
+      "rgba(147,213,186,1)",
+      "rgba(85,188,221,1)",
+      "rgba(76,128,175,1)",
+      "rgba(62,88,137,0)"
+    ]
+  }
+};
+
