639612fc49c6a000352504477bf3332d569a1479
Eric Lathrop <eric@ericlathrop.com>
2017-01-22 15:49:38 -0500

    Move scores to side of field

--- src/systems/renderer/draw-scores.js
+++ src/systems/renderer/draw-scores.js
@@ -1,11 +1,19 @@
 var constants = require("../../constants");
+var coordinateToScreen = require("../../coordinate-to-screen");
 
 module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
   ecs.addEach(function(entity, elapsed) { // eslint-disable-line no-unused-vars
     var score = game.entities.getComponent(entity, "score");
+    var player = game.entities.getComponent(constants.network, "network").player;
+
+    game.context.setTransform(1, 0, 0, 1, constants.screenWidth / 2, 0);
 
     game.context.fillStyle = "#000000";
-    game.context.fillText(score.player1, 50, constants.screenHeight - 25);
-    game.context.fillText(score.player2, constants.screenWidth - 50, constants.screenHeight - 25);
+    var p = coordinateToScreen(-30, (constants.screenHeight / 4));
+    game.context.fillText(player === 1 ? score.player2 : score.player1, p.x, p.y);
+    p = coordinateToScreen(-30, (constants.screenHeight / 4) * 3);
+    game.context.fillText(player === 1 ? score.player1 : score.player2, p.x, p.y);
+
+    game.context.setTransform(1, 0, 0, 1, 0, 0);
   }, "score");
 };
