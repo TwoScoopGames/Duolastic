1e47ba04ad35a54233868bb23f8591ae4d4f6f8d
Alex Bezuska <abezuska@19216811.local>
2017-01-22 15:17:44 -0500

    add ball art

--- src/systems/renderer/draw-circle.js
+++ src/systems/renderer/draw-circle.js
@@ -24,7 +24,7 @@ function draw(game, entity) {
 
   var ball = game.entities.getComponent(entity, "ball");
   if (ball) {
-    drawCircle(game.context, position.x, position.y, circle.radius, "rgba(50, 50, 50, 1)");
+    drawBall(game.context, position, circle);
   } else {
     drawStack(game.context, position, circle);
     // drawCircle(game.context, position.x, position.y, circle.radius, "rgba(255, 50, 50, 1)");
@@ -63,3 +63,78 @@ function drawCircle(ctx, x, y, radius, color) {
   ctx.fill();
 }
 
+
+
+function drawBall(ctx, position, circle) {
+
+  var config = {};
+  config.x = position.x;
+  config.y = position.y;
+  config.radius = circle.radius;
+  config.color = [118, 118, 118, 1];
+  config.insetColor = [50, 50, 50, 1];
+  config.outsetColor = [0, 0, 0, 0.5];
+  config.highlightColor = [190, 190, 190, 1];
+  config.highlightInsetColor = [118, 118, 118, 1];
+  config.highlightOutsetColor = [118, 118, 118, 1];
+  config.shadowColor = [0, 0, 0, 0.8];
+  config.shadowOutsetColor = [0, 0, 0, 0.8];
+  config.shadowInsetColor = [0, 0, 0, 0.8];
+  config.debugLines = false;
+  config.debugLineColor = [0, 0, 0, 1];
+
+
+  config.insetSize = config.radius * 0.46;
+  config.outsetSize = 0;
+  config.hightlightOffsetY = config.radius * -0.24;
+  config.highlightRadius = config.radius * 0.48;
+  config.hightlightOffsetX = config.radius * 0.18,
+  config.highlightInsetSize = config.radius * 0.36;
+  config.highlightOutsetSize = config.radius * 0.3;
+  config.shadowRadius = config.radius * 0.56;
+  config.shadowOffsetX = 0;
+  config.shadowOffsetY = config.radius * 5.48;
+  config.shadowOutsetSize = config.radius * 0.4;
+  config.shadowInsetSize = config.radius * 0.2;
+
+
+  // var shadowOffsetX = config.x + config.shadowOffsetX;
+  // var shadowOffsetY = config.y + config.shadowOffsetY;
+
+  // ctx.setTransform(1, 0, 0, 0.5, 0, 0);
+  // drawShadowCircle(ctx,
+  //   shadowOffsetX,
+  //   shadowOffsetY,
+  //   config.shadowRadius,
+  //   config.shadowColor,
+  //   config.shadowInsetSize,
+  //   config.shadowInsetColor,
+  //   config.shadowOutsetSize,
+  //   config.shadowOutsetColor);
+
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+  drawShadowCircle(ctx,
+      config.x,
+      config.y,
+      config.radius,
+      config.color,
+      config.insetSize,
+      config.insetColor,
+      config.outsetSize,
+      config.outsetColor);
+
+  var hightlightOffsetX = config.x + config.hightlightOffsetX;
+  var hightlightOffsetY = config.y + config.hightlightOffsetY;
+
+  ctx.setTransform(1, 0, 0, config.perspective, 0, 0);
+  drawShadowCircle(ctx,
+        hightlightOffsetX,
+        hightlightOffsetY,
+        config.highlightRadius,
+        config.highlightColor,
+        config.highlightInsetSize,
+        config.highlightInsetColor,
+        config.highlightOutsetSize,
+        config.highlightOutsetColor);
+
+}
