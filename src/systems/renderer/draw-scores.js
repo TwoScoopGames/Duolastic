571a80aef3c581656e2c40fdca8d4ccd385e184f
Eric Lathrop <eric@ericlathrop.com>
2017-01-23 21:46:40 -0500

    Make scores bigger

--- src/systems/renderer/draw-scores.js
+++ src/systems/renderer/draw-scores.js
@@ -8,10 +8,11 @@ module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 
     game.context.setTransform(1, 0, 0, 1, constants.screenWidth / 2, 0);
 
+    game.context.font = "50px sans-serif";
     game.context.fillStyle = "#000000";
-    var p = coordinateToScreen(-30, (constants.screenHeight / 4));
+    var p = coordinateToScreen(-50, (constants.screenHeight / 4));
     game.context.fillText(player === 1 ? score.player2 : score.player1, p.x, p.y);
-    p = coordinateToScreen(-30, (constants.screenHeight / 4) * 3);
+    p = coordinateToScreen(-50, (constants.screenHeight / 4) * 3);
     game.context.fillText(player === 1 ? score.player1 : score.player2, p.x, p.y);
 
     game.context.setTransform(1, 0, 0, 1, 0, 0);
