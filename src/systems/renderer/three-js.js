4ce8acc63b932b6a6b28eff121e3da9471fcfe3c
Eric Lathrop <eric@ericlathrop.com>
2017-01-31 19:35:42 -0500

    Add cylinders

--- src/systems/renderer/three-js.js
+++ src/systems/renderer/three-js.js
@@ -40,6 +40,50 @@ module.exports = function(ecs, game) { // eslint-disable-line no-unused-vars
 };
 
 function makeMesh(name, options) { // eslint-disable-line no-unused-vars
+  switch (name) {
+  case "cylinder":
+    return makeCylinder(options);
+  case "box":
+  default:
+    return makeBox(options);
+  }
+}
+
+function makeCylinder(options) {
+  var radiusTop = options.radiusTop;
+  if (radiusTop === undefined) {
+    radiusTop = 1;
+  }
+  var radiusBottom = options.radiusBottom;
+  if (radiusBottom === undefined) {
+    radiusBottom = 1;
+  }
+  var height = options.height;
+  if (height === undefined) {
+    height = 1;
+  }
+  var radiusSegments = options.radiusSegments;
+  if (radiusSegments === undefined) {
+    radiusSegments = 64;
+  }
+  var color = options.color;
+  if (typeof color === "string") {
+    color = parseInt(color, 16);
+  }
+  if (color === undefined) {
+    color = 0xffffff;
+  }
+
+  var geometry = new THREE.CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments);
+  var material = new THREE.MeshBasicMaterial({ color: color });
+  var mesh = new THREE.Mesh(geometry, material);
+
+  mesh.quaternion.setFromAxisAngle(new THREE.Vector3(1, 0, 0), Math.PI / 2);
+
+  return mesh;
+}
+
+function makeBox(options) { // eslint-disable-line no-unused-vars
   var width = options.width;
   if (width === undefined) {
     width = 1;
@@ -62,6 +106,5 @@ function makeMesh(name, options) { // eslint-disable-line no-unused-vars
 
   var geometry = new THREE.BoxGeometry(width, height, depth);
   var material = new THREE.MeshBasicMaterial({ color: color });
-  var cube = new THREE.Mesh(geometry, material);
-  return cube;
+  return new THREE.Mesh(geometry, material);
 }
