33fe04038df8b12942dfaf7a2e2bc8918140f7b4
Eric Lathrop <eric@ericlathrop.com>
2017-02-21 21:27:47 -0500

    Add text rendering

--- src/make-mesh.js
+++ src/make-mesh.js
@@ -11,6 +11,8 @@ module.exports = function makeMesh(name, options) { // eslint-disable-line no-un
     return makeCylinder(options);
   case "directionalLight":
     return makeDirectionalLight(options);
+  case "text":
+    return makeText(options);
   }
 };
 
@@ -80,4 +82,67 @@ function makeDirectionalLight(options) {
   light.shadowDarkness = 1;
   light.target.position.set(targetX, targetY, targetZ);
   return light;
+}
+
+var textCanvas = document.createElement("canvas");
+var textContext = textCanvas.getContext("2d");
+textCanvas.width = 256;
+textCanvas.height = 256;
+
+// var textMeasurementDiv = document.createElement("div");
+// textMeasurementDiv.style.display = "block";
+// textMeasurementDiv.style.position = "absolute";
+// textMeasurementDiv.style.top  = "-9999px";
+// textMeasurementDiv.style.left = "-9999px";
+// document.body.appendChild(textMeasurementDiv);
+
+// function measureTextInDiv(text, font) {
+//   textMeasurementDiv.style.font = font;
+//   textMeasurementDiv.textContent = text;
+//   return {
+//     width: textMeasurementDiv.offsetWidth,
+//     height: textMeasurementDiv.offsetHeight
+//   };
+// }
+
+function makeText(options) {
+  var text = getOption(options.text, "Hello, world!");
+  var fillStyle = getOption(options.fillStyle, "rgba(255, 0, 0, 0.95)");
+  var font = getOption(options.font, "40px sans-serif");
+  var textWidth = getOption(options.textWidth, textCanvas.textWidth);
+  var textHeight = getOption(options.textHeight, textCanvas.textHeight);
+  var width = getOption(options.width, textCanvas.width);
+  var height = getOption(options.height, textCanvas.height);
+
+  if (textCanvas.width < textWidth) {
+    textCanvas.width = textWidth;
+  }
+  if (textCanvas.height < textHeight) {
+    textCanvas.height = textHeight;
+  }
+  textContext.clearRect(0, 0, textCanvas.width, textCanvas.height);
+  textContext.textBaseline = "hanging";
+  textContext.font = font;
+  textContext.fillStyle = fillStyle;
+  textContext.fillText(text, 0, 0);
+
+  var texture1 = new THREE.Texture(textCanvas);
+  texture1.needsUpdate = true;
+
+  var material = new THREE.MeshBasicMaterial({ map: texture1, side: THREE.DoubleSide });
+  material.transparent = true;
+
+  var geometry = new THREE.PlaneGeometry(width, height);
+
+  geometry.faceVertexUvs[0] = [];
+  var maxU = textWidth / textCanvas.width;
+  var maxV = textHeight / textCanvas.height;
+  var bl = new THREE.Vector2(0, 1 - maxV);
+  var br = new THREE.Vector2(maxU, 1 - maxV);
+  var tr = new THREE.Vector2(maxU, 1);
+  var tl = new THREE.Vector2(0, 1);
+  geometry.faceVertexUvs[0][0] = [ tl, bl, tr ];
+  geometry.faceVertexUvs[0][1] = [ bl, br, tr ];
+
+  return new THREE.Mesh(geometry, material);
 }
