e5f8a2d315db292cd0eab9c4d70dbef96251524b
Eric Lathrop <eric@ericlathrop.com>
2017-02-23 19:32:04 -0500

    Revert changes to apply-movement-2d-analog

--- src/systems/simulation/apply-movement-2d-analog.js
+++ src/systems/simulation/apply-movement-2d-analog.js
@@ -1,34 +1,17 @@
-var constants = require("../../constants");
 module.exports = function(ecs, game) {
   game.entities.registerSearch("applyMovement2dAnalog", ["velocity", "movement2dAnalog"]);
   ecs.addEach(function applyMovement2dAnalog(entity) {
     var velocity = game.entities.getComponent(entity, "velocity");
     var movement2dAnalog = game.entities.getComponent(entity, "movement2dAnalog");
-    var networkRole = game.entities.getComponent(constants.network, "network").role;
-    var youArePlayer1 = networkRole === "server";
 
+    velocity.x += movement2dAnalog.magnitude * movement2dAnalog.accel * Math.cos(movement2dAnalog.angle);
+    velocity.y += movement2dAnalog.magnitude * movement2dAnalog.accel * Math.sin(movement2dAnalog.angle);
 
-    var velocityY = movement2dAnalog.magnitude * movement2dAnalog.accel * Math.sin(movement2dAnalog.angle);
-    var velocityX = movement2dAnalog.magnitude * movement2dAnalog.accel * Math.cos(movement2dAnalog.angle);
-
-    var magnitude, angle;
-
-    if (youArePlayer1) {
-      velocity.x -= velocityX;
-      velocity.y += velocityY;
-      console.log("Player1:", velocity.x, velocity.y);
-    } else {
-      velocity.x += velocityX;
-      velocity.y += velocityY;
-      console.log("Player2:", velocity.x, velocity.y);
-    }
-
-    magnitude = Math.sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y));
+    var magnitude = Math.sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y));
     if (magnitude > movement2dAnalog.max) {
-      angle = Math.atan2(velocity.y, velocity.x);
+      var angle = Math.atan2(velocity.y, velocity.x);
       velocity.x = movement2dAnalog.max * Math.cos(angle);
       velocity.y = movement2dAnalog.max * Math.sin(angle);
     }
-
   }, "applyMovement2dAnalog");
 };
