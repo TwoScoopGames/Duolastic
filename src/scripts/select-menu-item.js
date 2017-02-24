903393dd6079c10e0e2a674248f0785ce107ebca
Eric Lathrop <eric@ericlathrop.com>
2017-02-23 21:34:07 -0500

    Delete log

--- src/scripts/select-menu-item.js
+++ src/scripts/select-menu-item.js
@@ -1,6 +1,5 @@
 
 module.exports = function(entity, game) { // eslint-disable-line no-unused-vars
-  console.log("select", entity);
   var model = game.entities.getComponent(entity, "model");
   model.options.fillStyle = "green";
   model.needsUpdate = true;
