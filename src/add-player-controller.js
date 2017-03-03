4a74f9f465931ce0503297b6a40a1fd42e5ea3eb
abezuska <abezuska@gmail.com>
2017-03-02 23:33:37 -0500

    players stop (no velocity) after win/lose

--- src/add-player-controller.js
+++ src/add-player-controller.js
@@ -2,6 +2,10 @@ var constants = require("./constants");
 
 module.exports = {
   player1: function(game) {
+    var velocity = game.entities.addComponent(constants.player1, "velocity");
+    velocity.x = 0;
+    velocity.y = 0;
+    velocity.z = 0;
     var playerController = game.entities.addComponent(constants.player1, "playerController2d");
     playerController.left = "right";
     playerController.right = "left";
@@ -9,6 +13,10 @@ module.exports = {
     playerControllerAnalog.xScale = -1;
   },
   player2: function(game) {
+    var velocity = game.entities.addComponent(constants.player2, "velocity");
+    velocity.x = 0;
+    velocity.y = 0;
+    velocity.z = 0;
     var playerController = game.entities.addComponent(constants.player2, "playerController2d");
     playerController.up = "down";
     playerController.down = "up";
