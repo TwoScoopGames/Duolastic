872d4a655b562b60a2400d591ff4bb69293cc056
Alex Bezuska <abezuska@gmail.com>
2017-04-11 21:40:53 -0400

    add angular velocity system (now working)

--- src/components/quaternion.js
+++ src/components/quaternion.js
@@ -4,13 +4,13 @@ module.exports = {
       x: 0,
       y: 0,
       z: 0,
-      w: 0
+      w: 1
     };
   },
   reset: function(quaternion) {
     quaternion.x = 0;
     quaternion.y = 0;
     quaternion.z = 0;
-    quaternion.w = 0;
+    quaternion.w = 1;
   }
 };
